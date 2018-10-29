86bcaa1a04f3fcbd6d38b965d06536dc4ff4b21e
Buck <bytao@icloud.com>
2018-10-29 14:16:58 +0800

    add occur

--- init.el
+++ init.el
@@ -170,3 +170,14 @@
 ;;set cursor style
 (setq-default cursor-type 'bar)
 (blink-cursor-mode t)
+
+;; (defun highlight-selected-window ()
+;;   "Highlight selected window with a different background color."
+;;   (walk-windows (lambda (w)
+;;                   (unless (eq w (selected-window)) 
+;;                     (with-current-buffer (window-buffer w)
+;;                       (buffer-face-set '(:background "#111"))))))
+;;   (buffer-face-set 'default)
+;;   )
+
+;; (add-hook 'buffer-list-update-hook 'highlight-selected-window)
