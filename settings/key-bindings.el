b14c0d10ae8d20fa484fea66d320ecf8c090ceb3
Buck <bytao@icloud.com>
2019-06-27 15:08:09 +0800

    up

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -162,7 +162,7 @@
 (global-set-key (kbd "s-2") 'split-window-below)
 (global-set-key (kbd "s-3") 'split-window-horizontally)
 
-(global-set-key (kbd "C-;") 'marker-visit-prev)
-(global-set-key (kbd "C-:") 'marker-visit-next)
+(global-set-key (kbd "C-;") 'marker-visit-next)
+(global-set-key (kbd "C-:") 'marker-visit-prev)
 
 (provide 'key-bindings)
